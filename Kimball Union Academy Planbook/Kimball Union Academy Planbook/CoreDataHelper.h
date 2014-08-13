d747e11221a862ede8b5dc21311742e5a2b58eca
Nate Lang <langn15@kua.org>
2014-08-13 12:54:54 -0400

    Now with core data working on Daily View, still need to get overview working but first I am going to preload the core data

---
+++ Kimball Union Academy Planbook/Kimball Union Academy Planbook/CoreDataHelper.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataHelper.h
+//  Kimball Union Academy Planbook
+//
+//  Created by Nathaniel Lang on 8/9/14.
+//  Copyright (c) 2014 Nate Lang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataHelper : NSObject
+
+@end
---
+++ Kimball Union Academy Planbook/Kimball Union Academy Planbook/CoreDataHelper.m
@@ -0,0 +1,13 @@
+//
+//  CoreDataHelper.m
+//  Kimball Union Academy Planbook
+//
+//  Created by Nathaniel Lang on 8/9/14.
+//  Copyright (c) 2014 Nate Lang. All rights reserved.
+//
+
+#import "CoreDataHelper.h"
+
+@implementation CoreDataHelper
+
+@end
